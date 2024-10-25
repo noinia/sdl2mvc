3c1ff9b732b7901fea1541749bae9a5b091efcd1
Frank Staals <frank@fstaals.net>
2024-10-25 18:53:13 +0200

    it does something

--- src/SDL2MVC.hs
+++ src/SDL2MVC.hs
@@ -4,7 +4,7 @@ module SDL2MVC
   ( main
   ) where
 
-import           Control.Concurrent.Async (mapConcurrently_)
+import           Control.Concurrent.Async (mapConcurrently_, concurrently, withAsync)
 import           Control.Concurrent.STM (atomically)
 import qualified Control.Concurrent.STM.TBQueue as Queue
 import           Control.Lens
@@ -13,113 +13,98 @@ import           Data.Text (Text)
 import           Diagrams
 import           Diagrams.Backend.Cairo
 import           Effectful
+import           GHC.Natural
 import qualified SDL
 
 --------------------------------------------------------------------------------
 
+data Reaction m model action = Reaction model [m action]
 
 
+data LoopAction action = Shutdown
+                       | Continue action
+
+type Handler m model action = model -> action -> Reaction m model (LoopAction action)
 
 
-type View action = QDiagram Cairo V2 Double Any
+noEff       :: model -> Reaction m model action
+noEff model = Reaction model []
 
-data Reaction effs model action = Reaction model [Eff effs action]
 
-data AppConfig effs model action =
-     AppConfig { _render             :: model -> View action
-                                     -- ^ the view function
-               , _appUpdate          :: model -> action -> Reaction effs model action
-               , _startupAction      :: action
-                                     -- ^ action to run on startup
-               , _interpretSDLEvent  :: SDL.Event -> action
-                                     -- ^ How to interpet SDL events
-               }
+data AppConfig m model action =
+  AppConfig { _appModel      :: model
+            , _handler       :: Handler m model action
+            , _startupAction :: action
+            , _liftSDLEvent  :: SDL.Event -> action
+            }
 
 makeLenses ''AppConfig
 
-data App model action =
-     App { _appData         :: model
-         , _window          :: SDL.Window
-         , _renderer        :: SDL.Renderer
+data App m model action =
+     App { _config          :: AppConfig m model action
+         , _windowRef       :: SDL.Window
+         , _rendererRef     :: SDL.Renderer
          -- , _texture         :: SDL.Texture
-         , _eventQueue      :: Queue.TBQueue action
+         , _eventQueue      :: Queue.TBQueue (LoopAction action)
           -- ^ the event queue we are using
          }
 
 makeLenses ''App
 
-
-data AppModel effs model action =
-     AppModel { _initialWindowTitle :: Text
-              , _theModel           :: model
-              , _appConfig          :: AppConfig effs model action
-              }
-
--- makeClassy ''AppModel
+--------------------------------------------------------------------------------
 
 
-data AppAction action = Skip
-                      -- | Quit
-                      -- | Render
-                      | SDLEvent SDL.Event
-                      | Custom action
-                      deriving (Eq, Functor, Foldable, Traversable)
-
--- handleAppAction       :: model -> AppAction action -> Reaction effs model action
--- handleAppAction model = \case
---   Skip -> Reaction model [] -- do nothing
---   Quit -> Reaction model [ do SDL.destroyWindow (model^.window) ]
-
-
+maxQueueSize :: Natural
+maxQueueSize = 1000
 
 
 --------------------------------------------------------------------------------
 
 
+
 -- | Initialize the app
-initializeSDLApp       :: model -- ^ initial model
-                       -> IO (App model)
-initializeSDLApp model = do
+initializeSDLApp        :: AppConfig m model action -> IO (App m model action)
+initializeSDLApp appCfg = do
   SDL.initializeAll
   let windowCfg = SDL.defaultWindow
   window'   <- SDL.createWindow "foo" windowCfg
   renderer' <- SDL.createRenderer window' (-1) SDL.defaultRenderer
   -- texture'  <- SDL.Cairo.createCairoTexture' renderer' window'
 
+
   -- initialize the event queue
+  let initialActions = [ Continue $ appCfg^.startupAction
+                       -- , AppAction WaitSDLEvent
+                       -- , AppAction . UIStateAction . UIState.Redraw $ (appCfg^.render) m
+                       ]
   queue  <- atomically $ do q <- Queue.newTBQueue maxQueueSize
                             mapM_ (Queue.writeTBQueue q) initialActions
                             pure q
 
-  pure $ App { _appData    = model
-             , _window     = window'
-             , _renderer   = renderer'
-             , _eventQueue = queue'
+  pure $ App { _config      = appCfg
+             , _windowRef   = window'
+             , _rendererRef = renderer'
+             , _eventQueue  = queue
              }
 
-
-defaultAppConfig :: AppConfig effs model (AppAction action)
-defaultAppConfig = AppConfig { _render            = renderView
-                             , _appUpdate         = defaultUpdate
-                             , _startupAction     = Skip
-                             , _interpretSDLEvent = SDLEvent
-                             }
-
-
-defaultUpdate       :: model -> AppAction action -> Reaction effs model (AppAction action)
-defaultUpdate model = undefined
-
-
-data LoopAction action = Shutdown
-                       | Continue action
-
-
-
 -- | Runs the app
-runApp     :: App model -> IO ()
-runApp app = go (app^.model)
+runApp     :: forall model action. App IO model action -> IO ()
+runApp app = startup (app^.config.appModel)
   where
     queue = app^.eventQueue
+
+
+
+    startup model = do withAsync awaitSDLEvent $ \_ ->
+                         go model
+
+
+    -- | indefinitely wait for SDL events, pushing them into our own event Queue
+    awaitSDLEvent = do e <- SDL.waitEvent
+                       atomically $ Queue.writeTBQueue queue
+                                      (Continue . (app^.config.liftSDLEvent) $ e)
+                       awaitSDLEvent
+
 
     -- | The main app loop; we read the next event from the queue, and
     -- then handle it. In turn, this may again trigger further event
@@ -130,44 +115,20 @@ runApp app = go (app^.model)
 
     handle       :: model -> LoopAction action -> IO ()
     handle model = \case
-      Shutdown     -> destroyWindow (model^.window) -- destory the window, and then quit
-      Continue act -> let Reaction model' effs = (app^.appUpdate) act
+      Shutdown     -> SDL.destroyWindow (app^.windowRef) -- destory the window, and then quit
+      Continue act -> let Reaction model' effs = (app^.config.handler) model act
                       in do runAll effs -- runs the effects, which may produce more actions
                             go model'  -- continue to thandle then ext event.
 
-    -- | Runs a bunch of IO actions that may schedule more actions
-    runAll :: [Eff
-
-      IO (Action action model)] -> IO ()
+    runAll :: [IO (LoopAction action)] -> IO ()
     runAll = mapConcurrently_ (>>= atomically . Queue.writeTBQueue queue)
-
-
-
-
-
-
-  do
-  SDL.showWindow (app^.window)
-  print "woei"
-
-
-
-
-
 
 
 main :: IO ()
 main = do
-  app <- initializeSDLApp $ AppModel { _initialWindowTitle = "SDL2MVC App "
-                                     , _theModel           = ()
-                                     , _appConfig          = defaultAppConfig
-                                     }
+  app <- initializeSDLApp $ AppConfig { _appModel      = ()
+                                      , _handler       = \model _ -> noEff model
+                                      , _startupAction = Nothing
+                                      , _liftSDLEvent  = Just
+                                      }
   runApp app
-
-
-
-
---------------------------------------------------------------------------------
-
-renderView :: model -> View (AppAction action)
-renderView = undefined
