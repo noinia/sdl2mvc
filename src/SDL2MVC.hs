f7f061ea1d12b1193f863b16dd91c7381a70a005
Frank Staals <frank@fstaals.net>
2024-10-26 13:16:17 +0200

    cairo rendering

--- src/SDL2MVC.hs
+++ src/SDL2MVC.hs
@@ -10,44 +10,64 @@ import qualified Control.Concurrent.STM.TBQueue as Queue
 import           Control.Lens
 import           Data.Semigroup (Any(..))
 import           Data.Text (Text)
-import           Diagrams
+import           Diagrams hiding (Render)
 import           Diagrams.Backend.Cairo
 import           Effectful
 import           GHC.Natural
+import           Linear
 import qualified SDL
+import           SDL2MVC.Cairo
+
+import Debug.Trace
 
 --------------------------------------------------------------------------------
 
 data Reaction m model action = Reaction model [m action]
 
+noEff       :: model -> Reaction m model action
+noEff model = Reaction model []
+
+infix <#
+
+model <# act = Reaction model [act]
+
+
 
 data LoopAction action = Shutdown
                        | Continue action
 
+data Render = Render
+
 type Handler m model action = model -> action -> Reaction m model (LoopAction action)
 
 
-noEff       :: model -> Reaction m model action
-noEff model = Reaction model []
+
+
+type View m action = SDL.Texture -> m action
 
 
 data AppConfig m model action =
-  AppConfig { _appModel      :: model
-            , _handler       :: Handler m model action
-            , _startupAction :: action
-            , _liftSDLEvent  :: SDL.Event -> LoopAction action
+  AppConfig { _appModel        :: model
+            , _handler         :: App m model action -> Handler m model action
+            , _startupAction   :: action
+            , _liftSDLEvent    :: SDL.Event -> LoopAction action
+            , _liftRenderEvent :: Render -> action
+            , _appRender       :: model -> View m action
             }
 
-makeLenses ''AppConfig
 
 data App m model action =
      App { _config          :: AppConfig m model action
          , _windowRef       :: SDL.Window
          , _rendererRef     :: SDL.Renderer
-         -- , _texture         :: SDL.Texture
+         , _textureRef      :: SDL.Texture
          , _eventQueue      :: Queue.TBQueue (LoopAction action)
           -- ^ the event queue we are using
          }
+
+
+makeLenses ''AppConfig
+
 
 makeLenses ''App
 
@@ -66,14 +86,18 @@ maxQueueSize = 1000
 initializeSDLApp        :: AppConfig m model action -> IO (App m model action)
 initializeSDLApp appCfg = do
   SDL.initializeAll
-  let windowCfg = SDL.defaultWindow
+  let windowCfg   = SDL.defaultWindow
+      rendererCfg = SDL.defaultRenderer { SDL.rendererType = SDL.SoftwareRenderer
+                                        }
   window'   <- SDL.createWindow "foo" windowCfg
-  renderer' <- SDL.createRenderer window' (-1) SDL.defaultRenderer
-  -- texture'  <- SDL.Cairo.createCairoTexture' renderer' window'
+  renderer' <- SDL.createRenderer window' (-1) rendererCfg
+  print "go"
+  texture'  <- createCairoTexture' renderer' window'
 
 
   -- initialize the event queue
-  let initialActions = [ Continue $ appCfg^.startupAction
+  let initialActions = [ Continue . (appCfg^.liftRenderEvent) $ Render
+                       , Continue $ appCfg^.startupAction
                        -- , AppAction WaitSDLEvent
                        -- , AppAction . UIStateAction . UIState.Redraw $ (appCfg^.render) m
                        ]
@@ -84,6 +108,7 @@ initializeSDLApp appCfg = do
   pure $ App { _config      = appCfg
              , _windowRef   = window'
              , _rendererRef = renderer'
+             , _textureRef  = texture'
              , _eventQueue  = queue
              }
 
@@ -124,19 +149,86 @@ runApp app = startup (app^.config.appModel)
     handle       :: model -> LoopAction action -> IO ()
     handle model = \case
       Shutdown     -> SDL.destroyWindow (app^.windowRef) -- destroy the window, and then quit
-      Continue act -> let Reaction model' effs = (app^.config.handler) model act
+      Continue act -> let Reaction model' effs = (app^.config.handler) app model act
                       in do runAll effs -- runs the effects, which may produce more actions
                             go model'  -- continue to thandle then ext event.
 
     runAll :: [IO (LoopAction action)] -> IO ()
     runAll = mapConcurrently_ (>>= atomically . Queue.writeTBQueue queue)
 
+--------------------------------------------------------------------------------
+
+-- | Handles a render action
+handleRender           :: MonadIO m
+                       => App m model action -> model -> Render
+                       -> Reaction m model (LoopAction action)
+handleRender app model = \case
+    Render -> model <# do let renderer = app^.rendererRef
+                              texture  = app^.textureRef
+                          -- sets color to white
+                          SDL.clear renderer
+                          -- now render
+                          act <- (app^.config.appRender) model texture
+                          SDL.copy renderer texture Nothing Nothing
+                          SDL.present renderer
+                          pure $ Continue act
+
+--------------------------------------------------------------------------------
+
+
+-- -- | draw on SDL texture with Render monad from Cairo
+-- withCairoTexture     :: SDL.Texture -> Render () -> IO ()
+-- withCairoTexture t m = withCairoTexture' t (\s -> renderWith s m)
+
+
+--------------------------------------------------------------------------------
+
+-- cairoDraw _ _ =
+
+
+
+--------------------------------------------------------------------------------
+
+myDraw            :: model -> View IO MyAction
+myDraw _ texture = do cairoDraw texture
+                      pure Skip
+
+  -- do SDL.rendererDrawColor renderer SDL.$= V4 0 0 255 255
+  --                      SDL.drawRect renderer (Just $ SDL.Rectangle origin (V2 200 100))
+  --                      pure Skip
+
+
+-- withRenderer :: Handler m model action'
+--              -> App m model action -> model -> Either Render action -> Handler m model action'
+-- withRenderer handler
+
+-- withDefaultRender :: Handler m model action -> App m model action -> Handler m model action
+-- withDefaultRender handler app model =
+
+
+--------------------------------------------------------------------------------
+
+data MyAction = RenderAction Render
+              | SDLEvent SDL.Event
+              | Skip
+
+myHandler app model = \case
+  RenderAction renderAct -> handleRender app model renderAct
+  SDLEvent e                   -> case SDL.eventPayload e of
+    SDL.MouseMotionEvent mouseData -> model <# do print $ SDL.mouseMotionEventPos mouseData
+                                                  pure $ Continue Skip
+    _                              -> noEff model
+  Skip                   -> noEff model
+
+--------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
-  app <- initializeSDLApp $ AppConfig { _appModel      = ()
-                                      , _handler       = \model _ -> noEff model
-                                      , _startupAction = Nothing
-                                      , _liftSDLEvent  = withDefaultSDLEvents Just
+  app <- initializeSDLApp $ AppConfig { _appModel        = ()
+                                      , _handler         = myHandler
+                                      , _startupAction   = Skip
+                                      , _liftSDLEvent    = withDefaultSDLEvents SDLEvent
+                                      , _liftRenderEvent = RenderAction
+                                      , _appRender       = myDraw
                                       }
   runApp app
---
+++ src/SDL2MVC/Cairo.hs
@@ -0,0 +1,66 @@
+module SDL2MVC.Cairo
+  ( createCairoTexture
+  , createCairoTexture'
+  , withCairoTexture
+
+  , cairoDraw
+  ) where
+
+import           Control.Monad.IO.Class
+import           Foreign.C.Types (CInt)
+import           Foreign.Ptr (castPtr)
+import qualified GI.Cairo.Render as Cairo
+import           Linear (V2(..))
+import qualified SDL
+
+--------------------------------------------------------------------------------
+
+-- ported from sdl2-cairo
+
+
+
+-- | create new texture for Cairo with given size
+createCairoTexture    :: SDL.Renderer -> V2 CInt -> IO SDL.Texture
+createCairoTexture r = SDL.createTexture r SDL.ARGB8888 SDL.TextureAccessStreaming
+
+-- | create new texture for Cairo with the size of the given window
+createCairoTexture'     :: SDL.Renderer -> SDL.Window -> IO SDL.Texture
+createCairoTexture' r w = do
+                            surf <- SDL.getWindowSurface w
+                            sz   <- SDL.surfaceDimensions surf
+                            createCairoTexture r sz
+
+-- | draw on SDL texture with Render monad from Cairo
+withCairoTexture     :: SDL.Texture -> Cairo.Render () -> IO ()
+withCairoTexture t m = withCairoTexture' t (\s -> Cairo.renderWith s m)
+
+-- | lock and unwrap SDL texture, get a Cairo surface, pass it to some function
+withCairoTexture' :: SDL.Texture -> (Cairo.Surface -> IO a) -> IO a
+withCairoTexture' t m = do
+    SDL.TextureInfo f _ w h <- SDL.queryTexture t
+    case mapFormat f of
+      Nothing -> error "ERROR: Invalid pixel format for cairo use!"
+      Just f' -> do
+        (pixels, pitch) <- SDL.lockTexture t Nothing
+        print "after lock"
+        ret <- Cairo.withImageSurfaceForData (castPtr pixels) f'
+                 (fromIntegral w) (fromIntegral h) (fromIntegral pitch) m
+        SDL.unlockTexture t
+        pure ret
+  where
+    mapFormat SDL.ARGB8888 = Just Cairo.FormatARGB32
+    mapFormat SDL.RGB888   = Just Cairo.FormatRGB24
+    mapFormat _            = Nothing
+
+
+cairoDraw   :: SDL.Texture -> IO ()
+cairoDraw t = withCairoTexture t $ do
+                  Cairo.setSourceRGB 200 100 150
+                  Cairo.rectangle 100 200 300 400
+                  liftIO $ print "rect"
+                  Cairo.stroke
+                  Cairo.fill
+                  -- Cairo.setSourceRGB 200 100 150
+                  -- Cairo.rectangle 100 200 300 400
+                  -- Cairo.stroke
+                  -- Cairo.fill
