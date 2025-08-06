da2006b63d46364bb6a11247acd9021b3de59f3c
Frank Staals <frank@fstaals.net>
2025-08-06 21:05:31 +0200

    some fiddling with rendering again

--- src/SDL2MVC.hs
+++ src/SDL2MVC.hs
@@ -7,12 +7,13 @@ module SDL2MVC
 import           Control.Concurrent.Async (mapConcurrently_, withAsync, uninterruptibleCancel)
 import           Control.Concurrent.STM (atomically)
 import qualified Control.Concurrent.STM.TBQueue as Queue
-import           Control.Lens
+import           Control.Lens hiding (elements)
+import           Data.Foldable (for_)
 import           Debug.Trace
 import           Effectful
 import           GHC.Natural
 import           Linear
-import           Linear.Affine
+import qualified Linear.Affine as Linear
 import qualified SDL
 import           SDL2MVC.App
 import           SDL2MVC.Cairo
@@ -21,13 +22,26 @@ import           SDL2MVC.Reaction
 import           SDL2MVC.Render
 
 
+import           Data.Colour
+import           Data.Colour.Names (red,blue,white,green,orange)
+import           Data.Colour.SRGB (RGB(..), toSRGB)
 import qualified GI.Cairo.Render as Cairo
+import qualified GI.Cairo.Render.Matrix as Cairo
+import           HGeometry.Ball
+import           HGeometry.Box
+import           HGeometry.Ext
+import           HGeometry.Matrix
+import           HGeometry.Point
+import           HGeometry.Transformation
+import           HGeometry.Triangle
+import           HGeometry.Vector
+import           HGeometry.Viewport
 
 --------------------------------------------------------------------------------
 -- * Model
 
 
-data MyModel = MyModel { _mousePosition :: !(Maybe (Point V2 Int))
+data MyModel = MyModel { _mousePosition :: !(Maybe (Linear.Point V2 Int))
                        }
              deriving (Show,Eq)
 
@@ -84,6 +98,86 @@ toolBarWidth = 16
 -- withCairoTexture t m = withCairoTexture' t (\s -> renderWith s m)
 
 
+
+
+
+type Attributes = AlphaColour Double
+fillColor = id
+
+
+toCairoMatrix   :: Real r => Matrix 3 3 r -> Cairo.Matrix
+toCairoMatrix m = case m&elements %~ realToFrac of
+  Matrix (Vector3
+          (Vector3 a b c)
+          (Vector3 d e f)
+          _
+         ) -> Cairo.Matrix a d b e c f
+
+renderIn           :: Real r => Viewport r -> Cairo.Render () -> Cairo.Render ()
+renderIn vp render = do
+                        Cairo.save
+                        Cairo.transform (vp^.worldToHost.transformationMatrix.to toCairoMatrix)
+                        render
+                        Cairo.restore
+
+
+disk          :: (Point_ point 2 r, Real r)
+              => Attributes -> Disk point -> Cairo.Render ()
+disk ats disk = do setColor (fillColor ats)
+                   let Point2 x y = disk^.center.asPoint
+                                    & coordinates %~ realToFrac
+                       r          = realToFrac $ disk^.squaredRadius
+                   Cairo.arc x y (sqrt r) 0 (2*pi)
+                   Cairo.fill
+
+toRGBA col = ( toSRGB $ col `Data.Colour.over` black
+             , alphaChannel col
+             )
+
+setColor col = let (RGB r g b, a) = toRGBA col
+               in Cairo.setSourceRGBA r g b a
+
+rectangle          :: (Point_ point 2 r, Real r)
+                   => Attributes -> Rectangle point -> Cairo.Render ()
+rectangle ats rect = do let Point2 x y  = rect^.minPoint.asPoint
+                                          & coordinates %~ realToFrac
+                            Vector2 w h = realToFrac <$> size rect
+                        setColor $ fillColor ats
+                        Cairo.rectangle x y w h
+                        Cairo.fill
+
+triangle         :: (Point_ point 2 r, Real r)
+                 => Attributes -> Triangle point -> Cairo.Render ()
+triangle ats tri = do let Triangle a b c = toPoints tri
+                      setColor $ fillColor ats
+                      Cairo.moveTo (a^.xCoord) (a^.yCoord)
+                      Cairo.lineTo (b^.xCoord) (b^.yCoord)
+                      Cairo.lineTo (c^.xCoord) (c^.yCoord)
+                      Cairo.closePath
+                      Cairo.fill
+
+toPoints :: (Functor f, Point_ point 2 r, Real r) => f point -> f (Point 2 Double)
+toPoints = fmap (\p -> p^.asPoint &coordinates %~ realToFrac)
+
+
+myRectangles = [ Rectangle (Point2 10 10)   (Point2 200 100) :+ opaque red
+               , Rectangle (Point2 100 100) (Point2 250 250) :+ opaque blue
+               ]
+
+myTriangles = [ Triangle origin (Point2 400 10) (Point2 430 30) :+ opaque orange
+              ]
+
+-- textLabel                    :: Attributes
+--                              -> Point 2 Double
+--                              -> Text
+--                              -> Cairo.Render ()
+-- textLabel ats (Point2 x y) t =
+
+
+-- triangle :: Attributes -> Triangle (Point 2 Double) -> Cairo.Render ()
+-- triangle
+
+
 --------------------------------------------------------------------------------
 
 -- cairoDraw _ _ =
@@ -118,16 +212,13 @@ myDraw model texture =
   --   pure Skip
   Skip <$ case fmap fromIntegral <$> model^.mousePosition of
     Nothing -> print "cursor outside screen"
-    Just (P (V2 x y)) -> do SDL.TextureInfo _ _ w h <- SDL.queryTexture texture
-                            withCairoTexture texture $ do
-                                    Cairo.setSourceRGB 1 1 1
-                                    Cairo.rectangle 0 0 (fromIntegral w) (fromIntegral h)
-                                    Cairo.fill
-                                    Cairo.setSourceRGB 1 0.5 0
-                                    Cairo.arc x y 2 0 (2*pi)
-                                    -- Cairo.rectangle x y 20 20
-                                    Cairo.fill
-
+    Just p  -> do SDL.TextureInfo _ _ w h <- SDL.queryTexture texture
+                  withCairoTexture texture $ do
+                    rectangle (opaque white) (Rectangle origin (Point2 w h))
+                    disk (opaque green) (Disk p 4)
+                    renderIn (flipY $ Vector2 w h) $ do
+                      for_ myRectangles $ \(r :+ ats) -> rectangle ats r
+                      for_ myTriangles  $ \(r :+ ats) -> triangle ats r
 
 --------------------------------------------------------------------------------
 
